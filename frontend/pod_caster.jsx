0b96dbdea46bd0705577de3e63080c44def3b0a3
cperea1995 <cperea1995@gmail.com>
2020-04-07 15:38:52 -0400

    redux store created

--- frontend/pod_caster.jsx
+++ frontend/pod_caster.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionUtil from './util/session/session_api_util'
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
 
     //TEST
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     window.signUp = SessionUtil.signUp;
     window.login = SessionUtil.login;
     window.logout = SessionUtil.logout;

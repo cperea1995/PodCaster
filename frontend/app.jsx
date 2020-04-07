777018a5b1ce896664280168747b67649ec937bd
cperea1995 <cperea1995@gmail.com>
2020-04-07 17:19:51 -0400

    header component and container created

---
+++ frontend/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderContainer from './components/header/header_container';
+
+const App = () => {
+    return (
+        <>
+            <header>
+                <h1>PodCaster</h1>
+                <HeaderContainer />
+            </header>
+        </>
+    );
+};
+
+export default App;
--- frontend/pod_caster.jsx
+++ frontend/pod_caster.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SessionUtil from './util/session/session_api_util'
+// import * as SessionUtil from './util/session/session_api_util'
 import configureStore from './store/store';
+import Root from './root';
+import {login, logout} from './actions/session_actions'
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
 
     //TEST
+    window.login = login;
+    window.logout = logout;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    window.signUp = SessionUtil.signUp;
-    window.login = SessionUtil.login;
-    window.logout = SessionUtil.logout;
+    // window.signUp = SessionUtil.signUp;
+    // window.login = SessionUtil.login;
+    // window.logout = SessionUtil.logout;
     //TEST
+
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>TEST</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });

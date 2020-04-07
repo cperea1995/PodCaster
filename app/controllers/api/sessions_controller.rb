589455dcb949c9504dc87865617731dded760c7f
cperea1995 <cperea1995@gmail.com>
2020-04-06 23:35:47 -0400

    users and sessions controller done

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email],params[:user][:password])
+
+        if @user
+            login!(@user)
+           render "api/users/show"
+        else
+            render json: ["No user"], status: 422
+        end
+    end
+
+    def destroy
+         if logged_in?
+
+            logout!
+            render "api/users/show"
+        else
+            render json: ["No User to Log Out"], status: 404
+        end
+    end
+end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,26 @@
 class Api::UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: @user.error.full_messages, status: 422
+    end
+
+    def show
+        @user = User.find(params[:id])
+
+        if @user
+            render "api/users/show"
+        else
+            render json: ["No user found"], status: 404
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end

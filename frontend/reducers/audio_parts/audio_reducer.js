107890655f8b6c7b8635765bdfe7ebe06365ecb9
cperea1995 <cperea1995@gmail.com>
2020-05-19 15:01:34 -0400

    create audio reducer

---
+++ frontend/reducers/audio_parts/audio_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_CURRENT_SONG } from '../../actions/audio_actions';
+
+const audioReducer = (state = {}, action ) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_SONG:
+            return Object.assign({}, state, action.songURL)
+        default:
+            return state;
+    };
+};
+
+export default audioReducer;
